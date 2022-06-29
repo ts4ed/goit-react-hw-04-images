70f28a908081ba99e8dc89a1efd1ca845271062f
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-29 21:25:57 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,114 +1,91 @@
-import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-// import s from './App.module.css';
-// import PropTypes from 'prop-types';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 import Button from './Button/Button';
 import pixabayAPI from '../Services/pixabay-api';
+import { useState, useEffect } from 'react';
 
-export default class App extends Component {
-  state = {
-    searchRequest: '',
-    images: [],
-    galleryPage: 1,
-    error: null,
-    isLoading: false,
-    showModal: null,
-  };
+export default function App() {
+  const [searchRequest, setSearchRequest] = useState('');
+  const [images, setImages] = useState([]);
+  const [galleryPage, setGalleryPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevSearch = prevState.searchRequest;
-    const currentSearch = this.state.searchRequest;
-    const prevGalleryPage = prevState.galleryPage;
-    const currentGalleryPage = this.state.galleryPage;
-
-    if (
-      prevSearch !== currentSearch ||
-      prevGalleryPage !== currentGalleryPage
-    ) {
-      this.updateImages();
-    }
-  }
-
-  updateImages() {
-    const { searchRequest, galleryPage } = this.state;
-    this.setState({ isLoading: true });
-
-    try {
-      pixabayAPI(searchRequest, galleryPage).then(data => {
-        if (!data.data.hits.length) {
-          return alert('There is no images found with that search request');
-        }
-        const mappedImages = data.data.hits.map(
-          ({ id, webformatURL, tags, largeImageURL }) => ({
-            id,
-            webformatURL,
-            tags,
-            largeImageURL,
-          })
-        );
-        this.setState({
-          images: [...this.state.images, ...mappedImages],
+  useEffect(() => {
+    const updateImages = (searchRequest, galleryPage) => {
+      setIsLoading(true);
+      try {
+        pixabayAPI(searchRequest, galleryPage).then(data => {
+          if (!data.data.hits.length) {
+            return alert('There is no images found with that search request');
+          }
+          const mappedImages = data.data.hits.map(
+            ({ id, webformatURL, tags, largeImageURL }) => ({
+              id,
+              webformatURL,
+              tags,
+              largeImageURL,
+            })
+          );
+          setImages(i => [...i, ...mappedImages]);
         });
-      });
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (searchRequest !== '' || galleryPage !== 1) {
+      updateImages(searchRequest, galleryPage);
     }
-  }
+  }, [searchRequest, galleryPage]);
 
-  handleSearchSubmit = searchRequest => {
-    this.setState({
-      searchRequest,
-      images: [],
-      galleryPage: 1,
+  const handleSearchSubmit = value => {
+    if (value !== searchRequest) {
+      setSearchRequest(value);
+      setImages([]);
+      setGalleryPage(1);
+      return;
+    }
+  };
+
+  const loadMore = () => {
+    setGalleryPage(galleryPage + 1);
+  };
+
+  const openModalImage = id => {
+    const image = images.find(image => image.id === id);
+    setShowModal({
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
     });
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      galleryPage: prevState.galleryPage + 1,
-    }));
+  const closeModalImage = () => {
+    setShowModal(null);
   };
 
-  showModalImage = id => {
-    const image = this.state.images.find(image => image.id === id);
-    this.setState({
-      showModal: {
-        largeImageURL: image.largeImageURL,
-        tags: image.tags,
-      },
-    });
-  };
-
-  closeModalImage = () => {
-    this.setState({ showModal: null });
-  };
-
-  render() {
-    const { images, isLoading, error, showModal } = this.state;
-    return (
-      <>
-        <Searchbar onSearch={this.handleSearchSubmit} />
-        {error && alert(`Whoops, something went wrong: ${error.message}`)}
-        {isLoading && <Loader color={'#3f51b5'} size={32} />}
-        {images.length > 0 && (
-          <>
-            <ImageGallery images={images} handlePreview={this.showModalImage} />
-            <Button loadMore={this.loadMore} />
-          </>
-        )}
-        {showModal && (
-          <Modal
-            lgImage={showModal.largeImageURL}
-            tags={showModal.tags}
-            closeModal={this.closeModalImage}
-          />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSearch={handleSearchSubmit} />
+      {error && alert(`Whoops, something went wrong: ${error.message}`)}
+      {isLoading && <Loader color={'#3f51b5'} size={32} />}
+      {images.length > 0 && (
+        <>
+          <ImageGallery images={images} handlePreview={openModalImage} />
+          <Button loadMore={loadMore} />
+        </>
+      )}
+      {showModal && (
+        <Modal
+          lgImage={showModal.largeImageURL}
+          tags={showModal.tags}
+          closeModal={closeModalImage}
+        />
+      )}
+    </>
+  );
 }
